dd773975abbdb791d03134692eedb830e26b7783
Ian Spielman <ian.spielman@nist.gov>
2015-06-12 16:43:39 -0400

    feature2: Updated to read old filees where sequence_index is not defined.  fills with NaN

--- dataframe_utilities.py
+++ dataframe_utilities.py
@@ -45,8 +45,11 @@ def get_nested_dict_from_shot(filepath):
                                     row[orientation][label][image][key] = val
         row['filepath'] = filepath
         row['agnostic_path'] = labscript_utils.shared_drive.path_to_agnostic(filepath)
-        row['sequence'] = asdatetime(h5_file.attrs['sequence_id'].split('_')[0])        
-        row['sequence_index'] = h5_file.attrs['sequence_index']    
+        row['sequence'] = asdatetime(h5_file.attrs['sequence_id'].split('_')[0])
+        try:
+            row['sequence_index'] = h5_file.attrs['sequence_index']
+        except:
+            row['sequence_index'] = float('nan')
         if 'script' in h5_file: 
             row['labscript'] = h5_file['script'].attrs['name']
         try:
