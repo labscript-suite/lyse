904a86bd1d44135bf5ac1cfc24a76f0af23070b1
Chris Billington <chrisjbillington@gmail.com>
2017-04-26 01:09:50 -0400

    feature3: Added license header to tempfile2clipboard.py

--- tempfile2clipboard.py
+++ tempfile2clipboard.py
@@ -1,3 +1,16 @@
+#####################################################################
+#                                                                   #
+# /tempfile2clipboard.py                                            #
+#                                                                   #
+# Copyright 2017, Monash University                                 #
+#                                                                   #
+# This file is part of the program lyse, in the labscript suite     #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
 import sys
 import os
 
