c4203f3741c972c0a3c4b96643d4cfc5e687bd44
Zak V <zakven@mit.edu>
2020-11-03 14:25:28 -0500

    Removed print statement from Run.save_results().

--- lyse/__init__.py
+++ lyse/__init__.py
@@ -385,7 +385,6 @@ class Run(object):
         names = args[::2]
         values = args[1::2]
         for name, value in zip(names, values):
-            print('saving %s =' % name, value)
             self.save_result(name, value, **kwargs)
             
     def save_results_dict(self, results_dict, uncertainties=False, **kwargs):
